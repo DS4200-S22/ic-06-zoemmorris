845b4562a0d45a2cfc54cfe359e8f835544642f1
zoemmorris <morris.z@northeastern.edu>
2022-02-10 13:22:58 -0500

    button creates random number

--- main.js
+++ main.js
@@ -10,5 +10,8 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let randNum = Math.random();
+    let buttonDiv = document.getElementById("random_num");
+    buttonDiv.innerHTML = randNum;
+    console.log(randNum); 
 }
